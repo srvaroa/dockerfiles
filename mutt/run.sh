28ce5f7ae1165b9623a9bc7b0d6050eeae0f5002
Galo Navarro <anglorvaroa@gmail.com>
2018-02-25 20:01:50 +0100

    mutt: fix typo in smtp
    Signed-off-by: Galo Navarro <anglorvaroa@gmail.com>

--- mutt/run.sh
+++ mutt/run.sh
@@ -2,7 +2,7 @@
 
 DOMAIN=$(echo $1 | cut -f2 -d@)
 
-EMAIL=$1 SMTP_SERVER="smpt.$DOMAIN" IMAP_SERVER="imap.$DOMAIN" \
+EMAIL=$1 SMTP_SERVER="smtp.$DOMAIN" IMAP_SERVER="imap.$DOMAIN" \
     docker run --rm -it -v /etc/localtime:/etc/localtime:ro \
     -e EMAIL -e SMTP_SERVER -e IMAP_SERVER \
     --name mutt mutt
