17ccb7fe8b83b04031b3616a362478e781b8a660
Galo Navarro <anglorvaroa@gmail.com>
2018-11-02 21:36:39 +0100

    mbsync: simpler script
    Signed-off-by: Galo Navarro <anglorvaroa@gmail.com>

--- mbsync/run.sh
+++ mbsync/run.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 function usage_and_exit {
-    echo >&2 "Usage: $0 <password_file>"
+    echo >&2 "MUTT_PW_FILE must be set"
     exit 1
 }
 
-if [ "$#" != 1 ] ; then
+if [ -z ${MUTT_PW_FILE} ] ; then
     usage_and_exit
 fi
 
-if [ -z $1 ] ; then
-    usage_and_exit
-fi
-
-export G_PASSWORD=$(gpg -q -d $1 | grep G= | cut -f2 -d=)
-export V_PASSWORD=$(gpg -q -d $1 | grep V= | cut -f2 -d=)
+export G_PASSWORD=$(gpg -q -d $MUTT_PW_FILE | grep G= | cut -f2 -d=)
+export V_PASSWORD=$(gpg -q -d $MUTT_PW_FILE | grep V= | cut -f2 -d=)
 
 docker run --rm \
     -e G_PASSWORD \
