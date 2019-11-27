7165d102ad884eef9aba900726cdbfceabfde62a
Galo Navarro <anglorvaroa@gmail.com>
2019-11-28 00:49:46 +0100

    mutt: delete
    Signed-off-by: Galo Navarro <anglorvaroa@gmail.com>

--- mutt/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-# usage: $0 email@account.com
-
-DOMAIN=$(echo $1 | cut -f2 -d@)
-
-EMAIL=$1 SMTP_SERVER="smtp.$DOMAIN" IMAP_SERVER="imap.$DOMAIN" \
-    docker run --rm -it -v /etc/localtime:/etc/localtime:ro \
-    -e EMAIL -e SMTP_SERVER -e IMAP_SERVER \
-    --name mutt mutt
